6f836b688013d89ebfd2402a5e465d3e2b8fee0a
Grace Aranico <grace.aranico@gmail.com>
2022-02-09 16:53:10 -0800

    fix(missing bracket): Add missing closing curly bracket to fix array formatting

---
+++ Utils/madLibsArray.js
@@ -0,0 +1,94 @@
+const madLibsArray = [
+    {
+        question: `${cast.name[0]} starred in ${title}.`,
+        answer: true,
+        movieId: id,
+    },
+    {
+        question: `${title}'s cast included ${cast.name[0]} .`,
+        answer: true,
+        movieId: id,
+    },
+    {
+        question: `${title} was released on ${release_date}.`,
+        answer: true,
+        movieId: id,
+    },
+    {
+        question: `${release_date} was the release date of ${title}.`,
+        answer: true,
+        movieId: id,
+    },
+    {
+        question: `${overview} describes what ${title} is about.`,
+        answer: true,
+        movieId: id,
+    },
+    {
+        question: `${title} is a movie about ${overview}`,
+        answer: true,
+        movieId: id,
+    },
+    {
+        question: `${title} is considered a ${genres.name}`,
+        answer: true,
+        movieId: id,
+    },
+
+    {
+        question: `${title} is categorized under the ${genres.name} genre.`,
+        answer: true,
+        movieId: id,
+    },
+    {
+        question: `${title}'s plot used to described as ${changes.items.original_value} genre.`,
+        answer: true,
+        movieId: id,
+    },
+    {
+        question: `Tom Hanks starred in Cool Runnings.`,
+        answer: false,
+        movieId: id,
+    },
+    {
+        question: `Breakfast at Tiffany's cast included Audrey Hemmingway.`,
+        answer: false,
+        movieId: id,
+    },
+    {
+        question: `Ghostbusters was released on May 12, 187.`,
+        answer: false,
+        movieId: id,
+    },
+    {
+        question: `September 2, 1996 was the release date of Scream.`,
+        answer: false,
+        movieId: id,
+    },
+    {
+        question: `"A brillant and gifted young man must find the most beautiful location on the planet to set up a new vacation home for his family." describes what the movie Dune is about.`,
+        answer: false,
+        movieId: id, 
+    },
+    {
+        question: `Aladdin is a movie about a young man in search of a magic carpet.`,
+        answer: false,
+        movieId: id,
+        
+    },
+    {
+        question: `Scanner Darkley is considered a romantic comedy.`,
+        answer: false,
+        movieId: id,
+    },
+    {
+        question: `Pulp Fiction is categorized under the "Documentary" genre.`,
+        answer: false,
+        movieId: id,
+    },
+    {
+        question: `La La Land's plot used to be described as "Two lovers are drawn together by their desire to do what is right.  They are faced with a growing need to dance into the night."`,
+        answer: false,
+        movieId: id,
+    }
+];
