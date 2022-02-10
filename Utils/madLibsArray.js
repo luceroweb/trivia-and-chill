7680d9bf98057318bd921c8399a51167f13bf76f
Grace Aranico <grace.aranico@gmail.com>
2022-02-09 17:16:28 -0800

    chore(edit questions): Remove questions that require a second endpoint #9

--- Utils/madLibsArray.js
+++ Utils/madLibsArray.js
@@ -1,60 +1,60 @@
 const madLibsArray = [
+    // {
+    //     question: `${cast.name[0]} starred in ${movies.title}.`, //get credits endpoint for cast.name
+    //     answer: true,
+    //     movieId: id,
+    // },
+    // {
+    //     question: `${title}'s cast included ${cast.name[0]} .`, //get credits endpoint for cast.name
+    //     answer: true,
+    //     movieId: id,
+    // },
     {
-        question: `${cast.name[0]} starred in ${title}.`,
+        question: `${movies.title} was released on ${movies.release_date}.`, //movie details
         answer: true,
-        movieId: id,
+        movieId: `${movies.id}`,
     },
     {
-        question: `${title}'s cast included ${cast.name[0]} .`,
+        question: `${movies.release_date} was the release date of ${movies.title}.`,
         answer: true,
-        movieId: id,
+        movieId: `${movies.id}`,
     },
     {
-        question: `${title} was released on ${release_date}.`,
+        question: `${movies.overview} describes what ${movies.title} is about.`,
         answer: true,
-        movieId: id,
-    },
-    {
-        question: `${release_date} was the release date of ${title}.`,
-        answer: true,
-        movieId: id,
-    },
-    {
-        question: `${overview} describes what ${title} is about.`,
-        answer: true,
-        movieId: id,
+        movieId: `${movies.id}`,
     },
     {
         question: `${title} is a movie about ${overview}`,
         answer: true,
-        movieId: id,
+        movieId: `${movies.id}`,
     },
-    {
-        question: `${title} is considered a ${genres.name}`,
-        answer: true,
-        movieId: id,
-    },
+    // {
+    //     question: `${title} is considered a ${genres.name}`,
+    //     answer: true,
+    //     movieId: id,
+    // },
 
-    {
-        question: `${title} is categorized under the ${genres.name} genre.`,
-        answer: true,
-        movieId: id,
-    },
-    {
-        question: `${title}'s plot used to described as ${changes.items.original_value} genre.`,
-        answer: true,
-        movieId: id,
-    },
-    {
-        question: `Tom Hanks starred in Cool Runnings.`,
-        answer: false,
-        movieId: id,
-    },
-    {
-        question: `Breakfast at Tiffany's cast included Audrey Hemmingway.`,
-        answer: false,
-        movieId: id,
-    },
+    // {
+    //     question: `${title} is categorized under the ${genres.name} genre.`, //get details
+    //     answer: true,
+    //     movieId: id,
+    // },
+    // {
+    //     question: `${title}'s plot used to described as ${changes.items.original_value} genre.`, //get changes endpoint
+    //     answer: true,
+    //     movieId: id,
+    // },
+    // {
+    //     question: `Tom Hanks starred in Cool Runnings.`,
+    //     answer: false,
+    //     movieId: id,
+    // },
+    // {
+    //     question: `Breakfast at Tiffany's cast included Audrey Hemmingway.`,
+    //     answer: false,
+    //     movieId: id,
+    // },
     {
         question: `Ghostbusters was released on May 12, 187.`,
         answer: false,
@@ -76,16 +76,16 @@ const madLibsArray = [
         movieId: id,
         
     },
-    {
-        question: `Scanner Darkley is considered a romantic comedy.`,
-        answer: false,
-        movieId: id,
-    },
-    {
-        question: `Pulp Fiction is categorized under the "Documentary" genre.`,
-        answer: false,
-        movieId: id,
-    },
+    // {
+    //     question: `Scanner Darkley is considered a romantic comedy.`,
+    //     answer: false,
+    //     movieId: id,
+    // },
+    // {
+    //     question: `Pulp Fiction is categorized under the "Documentary" genre.`,
+    //     answer: false,
+    //     movieId: id,
+    // },
     {
         question: `La La Land's plot used to be described as "Two lovers are drawn together by their desire to do what is right.  They are faced with a growing need to dance into the night."`,
         answer: false,
