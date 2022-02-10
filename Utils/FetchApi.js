96bf8b0dfe1afa12a2053adfc819cd95b9d35d7c
Eyobel Legese <eyobel7k@gmail.com>
2022-02-10 14:22:35 -0800

    chore(API_endpoint): add api endpoints

--- Utils/FetchApi.js
+++ Utils/FetchApi.js
@@ -2,26 +2,27 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { View, Text } from "react-native";
 
-
-
 export default function FetchApi() {
   const [movies, setMovies] = useState([]);
-  const randomNumber = Math.floor(Math.random() * 20);
+  const randomNumber = Math.floor(Math.random() * 1.0);
+  const Video_Trailer =
+    "https://api.themoviedb.org/3/movie/634649/videos?api_key=68f693d561359896a6ae9c926dc4f853&language=en-US";
+  ("https://api.themoviedb.org/3/movie/936979/videos?api_key=68f693d561359896a6ae9c926dc4f853&language=en-US");
+  const Popular_Movies =
+    "https://api.themoviedb.org/3/movie/popular?api_key=68f693d561359896a6ae9c926dc4f853&language=en-US&page=1";
   useEffect(
     () =>
       axios
-        .get(
-          "https://api.themoviedb.org/3/movie/popular?api_key=68f693d561359896a6ae9c926dc4f853&language=en-US&page="
-          
-        )
-        .then((res) => setMovies(res.data.results[randomNumber].title))
+        .get(Popular_Movies)
+        .then((res) => console.log(res.data.results[randomNumber].id))
         .catch((err) => console.log(err.response.data)),
-
     []
   );
- console.log(movies)
+
+  console.log(movies);
   return (
-  <View >
-     <Text>{movies}</Text>
-  </View>);
+    <View>
+      <Text>{movies}</Text>
+    </View>
+  );
 }
