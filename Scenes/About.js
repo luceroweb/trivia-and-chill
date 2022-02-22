59deb0f339695dd614f0dec5e989ce06bc3f8765
Garet Hough <garetshough@gmail.com>
2022-02-22 14:58:32 -0800

    chore(about): creating basic about page

---
+++ Scenes/About.js
@@ -0,0 +1,25 @@
+import { View, Text } from "react-native";
+
+const About = () => {
+  return (
+    <View>
+      <Text>About the "Guess The Movie" Game:</Text>
+
+      <Text>
+        This game was created in order for our developer team [link to
+        "Developer Credits" scene] to learn more about React-Native, Redux,
+        Axios, and dynamic question generation using a live data base.
+      </Text>
+
+      <Text>About Workforce Training:</Text>
+
+      <Text>
+        Our curriculum doesn’t come from textbooks, it comes directly from
+        practicing industry experts. Our system of teaching is hands-on and
+        up-to-date with current practices. Learn more
+      </Text>
+    </View>
+  );
+};
+
+export default About;
