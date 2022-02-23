15a69232f784654fb498d58612465b41d1182722
Myles <mazehko@alphaworks.tech>
2022-02-23 12:34:01 -0800

    Google font Limelight install

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import { StyleSheet, View } from "react-native";
 import Nav from "./Components/Nav";
 import { store } from "./Utils/store";
 import Badge from "./Components/Badge";
+import { useFonts, Limelight_400Regular } from '@expo-google-fonts/limelight';
 
 export default function App() {
   return (
