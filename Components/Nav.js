bd168bec3085bd35e963bd170ced34d8152c70f6
Xavier Mercado <xmercado1576@gmail.com>
2022-02-23 12:40:51 -0800

    fix(nav): adjust scrollview to flex 1

--- Components/Nav.js
+++ Components/Nav.js
@@ -11,7 +11,7 @@ function Nav({ scene }) {
   return (
     <SafeAreaView style={styles.layout}>
       <Header style={styles.header} />
-      <ScrollView style={styles.scrollView}>
+      <ScrollView style={styles.scrollView} contentContainerStyle={{flex: 1}}>
         {scene === "Main" && <Main />}
         {scene === "GameOver" && <GameOver />}
         {scene === "Question" && <Question />}
@@ -32,7 +32,7 @@ const styles = StyleSheet.create({
     height: "10%",
   },
   scrollView: {
-    height: "80%",
+    height: "100%",
   },
   footer: {
     height: "10%",
