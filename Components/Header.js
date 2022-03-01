2fa512de8905db414039f4e09ad37110b2a147d3
Myles <mazehko@alphaworks.tech>
2022-03-01 09:37:39 -0800

    pull latest code to update limelight font

--- Components/Header.js
+++ Components/Header.js
@@ -1,16 +1,6 @@
 import { StyleSheet, View, Text, Pressable } from "react-native";
 import { connect } from "react-redux";
 
-<<<<<<< HEAD
-function Header(){
-    return(
-        <View style={styles.container}>
-            <Text>
-            ABOUT TEAM  |  PROJECT GITHUB  |  BITWISE WORFORCE TRAINING WEBSITE  |  BITWISE WORKFORCE TRAINING TWITTER
-            </Text>
-        </View>
-    )
-=======
 function Header() {
   return (
     <View style={styles.container}>
@@ -20,27 +10,15 @@ function Header() {
       </Text>
     </View>
   );
->>>>>>> a1c12b7328d8b3b6abf8e9c4040e836eb79b0c04
 }
 
 const styles = StyleSheet.create({
-<<<<<<< HEAD
-    container: {
-        flex: 1,
-        backgroundColor: 'red',
-        height: 100,
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-=======
   container: {
     backgroundColor: "#EEF525",
     width: "100%",
     alignItems: "center",
     justifyContent: "center",
   },
->>>>>>> a1c12b7328d8b3b6abf8e9c4040e836eb79b0c04
 });
 
 export default connect()(Header);
