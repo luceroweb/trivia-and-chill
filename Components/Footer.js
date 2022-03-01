d0e2e3cc638105cb1565da18eb97c6054b9bc96a
Myles <mazehko@alphaworks.tech>
2022-03-01 09:36:54 -0800

    Update Footer.js

--- Components/Footer.js
+++ Components/Footer.js
@@ -10,23 +10,12 @@ function Footer() {
 }
 
 const styles = StyleSheet.create({
-<<<<<<< HEAD
-    container: {
-        flex: 1,
-        backgroundColor: 'red',
-        height: 100,
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-=======
   container: {
     backgroundColor: "#EEF525",
     width: "100%",
     alignItems: "center",
     justifyContent: "center",
   },
->>>>>>> a1c12b7328d8b3b6abf8e9c4040e836eb79b0c04
 });
 
 export default connect()(Footer);
