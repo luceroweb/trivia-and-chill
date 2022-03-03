19879aa46b5c79c34caeab6885569b854cbda52d
Garet Hough <garetshough@gmail.com>
2022-03-02 17:01:00 -0800

    feat(timer): added clock component to display the clock ui within the timer.js file

--- Components/Timer.js
+++ Components/Timer.js
@@ -3,6 +3,7 @@ import { StyleSheet, View, Text} from 'react-native';
 import { connect } from 'react-redux';
 import { Audio } from 'expo-av';
 import tick from '../Sounds/tick.wav';
+import Clock from "../Components/Clock";
 
 function Timer({ setScene, timerCount, setTimerCount }) {
   const [sound, setSound] = useState();
@@ -39,7 +40,7 @@ function Timer({ setScene, timerCount, setTimerCount }) {
 
   return (
     <View style={styles.timer}>
-      <Text>{timerCount}</Text>
+      <Clock/>
     </View>
   )
 }
@@ -67,6 +68,6 @@ const styles = StyleSheet.create({
         position: 'absolute',
         left: 0,
         top: 0,
-        flex: 1
+        flex: 1,
     }
 });
