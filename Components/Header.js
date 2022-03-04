d54d3ef5ae71f2b80e3d1e76296f7c8f3576f63a
Juan Lucero <jlucero@alhpaworks.tech>
2022-03-04 10:44:29 -0800

    feat(header): add header #45

--- Components/Header.js
+++ Components/Header.js
@@ -1,31 +1,30 @@
-import { StyleSheet, View, Text, Pressable, Platform} from "react-native";
+import { StyleSheet, View, Text, Pressable, Platform } from "react-native";
 import { connect } from "react-redux";
-import AppLoading from 'expo-app-loading';
-import { useFonts, Limelight_400Regular } from '@expo-google-fonts/limelight';
+import AppLoading from "expo-app-loading";
+import { useFonts, Limelight_400Regular } from "@expo-google-fonts/limelight";
 
 function Header() {
   let [fontsLoaded] = useFonts({
-		Limelight_400Regular,
-	});
-	if (!fontsLoaded) {
-
-		return <AppLoading />;
-	} else {  return (
-
-    <View style={styles.container}>
-    <Text
-        style={{
+    Limelight_400Regular,
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text
+          style={{
             color: "#F2D379",
             fontFamily: "Limelight_400Regular",
             fontSize: 30,
-               }}
-                >
-             The Movie Game
-      </Text>
-  
-     </View> 
-   );
-}}
+          }}
+        >
+          The Movie Game
+        </Text>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -33,29 +32,31 @@ const styles = StyleSheet.create({
     width: "100%",
     alignItems: "center",
     justifyContent: "center",
-    borderBottomWidth:3,
-    borderColor: '#F2D379'
+    borderBottomWidth: 3,
+    borderColor: "#F2D379",
+    backgroundColor: "#401323",
   },
   movieGame: {
     fontSize: 60,
-    textAlign: 'center',
-    fontFamily: 'Limelight_400Regular',
-    color: '#F2D379',
+    textAlign: "center",
+    fontFamily: "Limelight_400Regular",
+    color: "#F2D379",
   },
   movieGameMobile: {
     fontSize: 40,
     marginBottom: 70,
-    textAlign: 'center',
-    fontFamily: 'Limelight_400Regular',
-    color: '#F2D379',
+    textAlign: "center",
+    fontFamily: "Limelight_400Regular",
+    color: "#F2D379",
   },
-  
+
   movieGameMini: {
     fontSize: 24,
-    marginBottom: Platform.OS === 'android' ? 20 : 70,
-    textAlign: 'center',
-    fontFamily: 'Limelight_400Regular',
-    color: '#F2D379',
-}});
+    marginBottom: Platform.OS === "android" ? 20 : 70,
+    textAlign: "center",
+    fontFamily: "Limelight_400Regular",
+    color: "#F2D379",
+  },
+});
 
 export default connect()(Header);
