1c802b38d68222a870e9af96cb14ad31753560bc
Juan Lucero <jlucero@alhpaworks.tech>
2022-03-04 10:19:03 -0800

    fix(youtubeid): prevent trailer load on missing youtubeid #50

--- Components/Clock.js
+++ Components/Clock.js
@@ -1,38 +1,39 @@
-import * as React from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
-import Constants from 'expo-constants';
-import { CountdownCircleTimer } from 'react-native-countdown-circle-timer';
+import * as React from "react";
+import { Text, View, StyleSheet, Button } from "react-native";
+import Constants from "expo-constants";
+import { CountdownCircleTimer } from "react-native-countdown-circle-timer";
 
 export default function App() {
-  const [isPlaying, setIsPlaying] = React.useState(true)
+  const [isPlaying, setIsPlaying] = React.useState(true);
 
   return (
     <View style={styles.container}>
       <CountdownCircleTimer
         isPlaying={isPlaying}
         duration={10}
-        colors={["#F2D379", "#401323"]}
+        colors={["#F2D379", "#CA3D45"]}
         colorsTime={[10, 0]}
         onComplete={() => ({ shouldRepeat: false, delay: 2 })}
         size={50}
         strokeWidth={6}
-    >
-      {({ remainingTime, color }) => (
-        <Text style={{ color, fontSize: 20 }}>
-          {remainingTime}
-        </Text>
-      )}
-    </CountdownCircleTimer>
-  </View>
-  )
+        trailColor={"#CA3D45"}
+      >
+        {({ remainingTime, color }) => (
+          <Text style={{ color: "#F2D379", fontSize: 20 }}>
+            {remainingTime}
+          </Text>
+        )}
+      </CountdownCircleTimer>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     paddingTop: Constants.statusBarHeight,
     padding: 8,
-  }
+  },
 });
