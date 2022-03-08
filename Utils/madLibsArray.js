4b7b39b79b1327913c7ecafb700e8448cef4ee39
Myles <mazehko@alphaworks.tech>
2022-03-08 09:43:08 -0800

    Update madLibsArray.js

--- Utils/madLibsArray.js
+++ Utils/madLibsArray.js
@@ -43,11 +43,7 @@ export default function madLibsArray(movies) {
       //   answer: true,
       //   movieId: `${movies.id}`,
       // },
-      // {
-      //   question: `${movies.title}'s plot used to described as ${movies.changes.items.original_value} genre.`, //get changes endpoint for original plot
-      //   answer: true,
-      //   movieId: `${movies.id}`,
-      // },
+      
       {
         question: `September 2, 1996 was the release date of Scream.`,
         answer: false,
@@ -73,7 +69,36 @@ export default function madLibsArray(movies) {
         answer: false,
         movieId: 620,
       },
-      
+      {
+        question: `${movies.title}'s plot used to described as ${changes.items.original_value}.`,
+        answer: true,
+        movieId: `${movies.id}`,
+      },
+      {
+        question: `${changes.items.original_value} was the original plot of ${movies.title}.`,
+        answer: true,
+        movieId: `${movies.id}`,
+      },
+      {
+        question: `Encanto's plot used to be described as "The Luceros are an extraordinary family who live hidden in the mountains of Mexico in a charmed place called the Encanto. The magic of the Encanto has blessed every child in the family with a unique gift -- every child except Miranda"`,
+        answer: false,
+        movieId: 568124, 
+      },
+      {
+        question: `Deformed since birth, a bitter man known only as the Phantom lives in the sewers underneath the Marid Opera House. He falls in love with the obscure chorus singer Christine, and privately tutors her while terrorizing the rest of the opera house and demanding Christine be given lead roles. Things get worse when Christine meets back up with her childhood acquaintance Raoul and the two fall in love`,
+        answer: false,
+        movieId: 9833,
+      },
+      {
+        question: `George and Kevin are two men whose stupidity is really indescribable. When Martha, a beautiful woman, loses an important suitcase with money before she leaves for Miami, the two friends (who have found the suitcase) decide to return it to her. After some "adventures" they finally get to Aspen where, using the lost money they live it up and fight for Mary's heart.`,
+        answer: false,
+        movieId: 8467,
+      },
+      {
+        question: `${title}'s plot used to described as ${changes.items.original_value}.`,
+        answer: true,
+        movieId: id,
+      }, 
     ]
   );
 }
