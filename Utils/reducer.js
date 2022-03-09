340920f74df5149c49688b53d4df0dd475b9c2fe
Grace Aranico <grace.aranico@gmail.com>
2022-03-09 12:40:54 -0800

    chore(commit): commit before pull

--- Utils/reducer.js
+++ Utils/reducer.js
@@ -37,40 +37,11 @@ export const reducer = (state = initialState, action) => {
         ...state,
         selectedMovie: {},
       };
+    case "SET_PERFORMER_NAME":
+      return {
+        ...state,
+        performerName: action.performerName
+      };
   }
-  
-  export const reducer = (state = initialState, action) => {
-    switch (action.type) {
-      case "INCREASE_WINNING_STREAK":
-        return {
-          ...state,
-          winningStreak: state.winningStreak + 1,
-        };
-      case "RESET_WINNING_STREAK":
-        return {
-          ...state,
-          winningStreak: 0,
-        };
-      case "SET_SCENE":
-        return {
-          ...state,
-          scene: action.name
-        };
-      case "SET_MOVIES":
-        return {
-          ...state,
-          movies: action.movies
-        };
-      case "SET_SELECTED_MOVIE":
-        return {
-          ...state,
-          selectedMovie: action.selectedMovie
-        };
-      case "SET_PERFORMER_NAME":
-        return {
-          ...state,
-          performerName: action.performerName
-        };
-    }
     return state;
   };
