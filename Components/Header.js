91b9afcf548becf4d7301c1d45b483094428f666
Garet Hough <garetshough@gmail.com>
2022-03-09 15:08:20 -0800

    feat(Header): Completed settings modal without functionality #25

--- Components/Header.js
+++ Components/Header.js
@@ -1,9 +1,14 @@
-import { StyleSheet, View, Text, Pressable, Platform } from "react-native";
+import { StyleSheet, Alert, Modal, View, Text, Pressable, Platform, } from "react-native";
+import {useState} from "react";
 import { connect } from "react-redux";
 import AppLoading from "expo-app-loading";
 import { useFonts, Limelight_400Regular } from "@expo-google-fonts/limelight";
+import { AntDesign } from '@expo/vector-icons';
 
 function Header() {
+
+  const [modalVisible, setModalVisible] = useState(false)
+
   let [fontsLoaded] = useFonts({
     Limelight_400Regular,
   });
@@ -21,6 +26,36 @@ function Header() {
         >
           The Movie Game
         </Text>
+
+        <View>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Hello World!</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <AntDesign name="arrowleft" size={24} color="black" />
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      <Pressable
+        style={[styles.button, styles.buttonOpen]}
+        onPress={() => setModalVisible(true)}
+      >
+        <AntDesign name="setting" size={24} color="#F2D379" />
+      </Pressable>
+    </View>
       </View>
     );
   }
@@ -35,6 +70,7 @@ const styles = StyleSheet.create({
     borderBottomWidth: 3,
     borderColor: "#F2D379",
     backgroundColor: "#401323",
+    flexDirection: "row",
   },
   movieGame: {
     fontSize: 60,
@@ -57,6 +93,39 @@ const styles = StyleSheet.create({
     fontFamily: "Limelight_400Regular",
     color: "#F2D379",
   },
+  gear:{
+  },
+
+  // testing modal
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  }
 });
 
 export default connect()(Header);
---
+++ Utils/SettingsModal.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Modal, StyleSheet, Text, View } from "react-native";
+
+const SettingsModal = () => {
+  return (
+    <div>SettingsModal</div>
+  )
+}
+
+export default SettingsModal
