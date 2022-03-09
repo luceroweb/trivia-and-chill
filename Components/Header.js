0e606aa297cf0328f1f5fe65694401878b296680
Garet Hough <garetshough@gmail.com>
2022-03-09 15:35:46 -0800

    fix(Header): removed previous modal stylings #25

--- Components/Header.js
+++ Components/Header.js
@@ -64,36 +64,6 @@ const styles = StyleSheet.create({
   gear:{
   },
 
-  // testing modal
-  modalView: {
-    margin: 20,
-    backgroundColor: "white",
-    borderRadius: 20,
-    padding: 35,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5
-  },
-  button: {
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2
-  },
-  textStyle: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center"
-  },
-  modalText: {
-    marginBottom: 15,
-    textAlign: "center"
-  }
 });
 
 export default connect()(Header);
