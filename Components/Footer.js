9a3605d913a51ddcf5ed01c0c142934d215d80ae
Juan Lucero <jlucero@alhpaworks.tech>
2022-03-31 11:48:03 -0700

    feat(howtoplay): Add how to play scene boilerplate

--- Components/Footer.js
+++ Components/Footer.js
@@ -2,45 +2,49 @@ import { StyleSheet, View, Text, Pressable } from "react-native";
 import { connect } from "react-redux";
 
 function Footer({ setScene }) {
-	return (
-		<View style={styles.container}>
-			<Pressable onPress={() => setScene("Credits")}>
-				<Text style={styles.textColor}>Credits</Text>
-			</Pressable>
-			<Text style={styles.textColor}> | </Text>
-			<Pressable onPress={() => setScene("About")}>
-				<Text style={styles.textColor}>About</Text>
-			</Pressable>
-		</View>
-	);
+  return (
+    <View style={styles.container}>
+      <Pressable onPress={() => setScene("Credits")}>
+        <Text style={styles.textColor}>Credits</Text>
+      </Pressable>
+      <Text style={styles.textColor}> | </Text>
+      <Pressable onPress={() => setScene("About")}>
+        <Text style={styles.textColor}>About</Text>
+      </Pressable>
+      <Text style={styles.textColor}> | </Text>
+      <Pressable onPress={() => setScene("HowToPlay")}>
+        <Text style={styles.textColor}>How To Play</Text>
+      </Pressable>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		width: "100%",
-		alignItems: "center",
-		justifyContent: "center",
-		borderTopWidth: 3,
-		padding: 5,
-		borderColor: "#F2D379",
-		backgroundColor: "#401323",
-		flexDirection: "row",
-	},
-	textColor: {
-		color: "#F2D379",
-		fontSize: 20,
-		textAlign: "center",
-	},
+  container: {
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    borderTopWidth: 3,
+    padding: 5,
+    borderColor: "#F2D379",
+    backgroundColor: "#401323",
+    flexDirection: "row",
+  },
+  textColor: {
+    color: "#F2D379",
+    fontSize: 20,
+    textAlign: "center",
+  },
 });
 
 function mapDispatchToProps(dispatch) {
-	return {
-		setScene: (name) =>
-			dispatch({
-				type: "SET_SCENE",
-				name,
-			}),
-	};
+  return {
+    setScene: (name) =>
+      dispatch({
+        type: "SET_SCENE",
+        name,
+      }),
+  };
 }
 
 export default connect(null, mapDispatchToProps)(Footer);
