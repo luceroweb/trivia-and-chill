76cc1b6ec919717f32840fd81671d8d53976fb5a
alberme <albertm@alphaworks.tech>
2022-04-05 10:09:50 -0700

    chore(QuestionInstructions): cleanup #122

--- Components/QuestionInstructions.js
+++ Components/QuestionInstructions.js
@@ -1,26 +1,27 @@
 import { ImageBackground, StyleSheet, View, Animated, useWindowDimensions, } from "react-native";
 import { Entypo } from "@expo/vector-icons";
 import { useEffect, useRef } from "react";
+
 export default function QuestionInstructions() {
   const { width } = useWindowDimensions();
   const translation = useRef(new Animated.ValueXY({ x: 250, y: 40 })).current;
 
   useEffect(() => {
     Animated.loop(
-        Animated.parallel([
-          Animated.timing(translation.x, {
-            toValue: 0,
-            duration: 2000,
-            delay: 1000,
-            useNativeDriver: true,
-          }),
-          Animated.timing(translation.y, {
-            toValue: width < 1385 ? width * 0.13 : 180,
-            duration: 2000,
-            delay: 1000,
-            useNativeDriver: true,
-          }),
-        ])  
+      Animated.parallel([
+        Animated.timing(translation.x, {
+          toValue: 0,
+          duration: 2000,
+          delay: 1000,
+          useNativeDriver: true,
+        }),
+        Animated.timing(translation.y, {
+          toValue: width < 1385 ? width * 0.13 : 180,
+          duration: 2000,
+          delay: 1000,
+          useNativeDriver: true,
+        }),
+      ])  
     ).start();
   }, [width]);
 
@@ -28,22 +29,23 @@ export default function QuestionInstructions() {
     <View style={styles.container}>
       <ImageBackground
         source={require("../Images/question-page.png")}
-        // resizeMode="cover"
-        style={{ width: '100%', height: '100%', aspectRatio: 1021 / 540, borderRadius: 20, zIndex: 100 }}
+        style={{ width: '100%', height: '100%', aspectRatio: 1021 / 540, borderRadius: 20 }}
       >
-        <Animated.Image source={require('../Images/question-answered.png')}
-        style={{ 
-          position: 'absolute',
-          width: '100%',
-          height: '100%',
-          aspectRatio: 1021 / 540,
-          borderRadius: 20, 
-          opacity: translation.x.interpolate({
-            inputRange: [0, 2],
-            outputRange: [1, 0],
-            extrapolate: 'clamp',
-          }),
-        }} />
+        <Animated.Image
+          source={require('../Images/question-answered.png')}
+          style={{ 
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+            aspectRatio: 1021 / 540,
+            borderRadius: 20, 
+            opacity: translation.x.interpolate({
+              inputRange: [0, 2],
+              outputRange: [1, 0],
+              extrapolate: 'clamp',
+            }),
+          }}
+        />
         <Animated.View
           style={{
             flex: 1,
