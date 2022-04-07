b03d1e3a0978501dab3da9a099ae8929add60668
alberme <albertm@alphaworks.tech>
2022-04-07 14:28:46 -0700

    chore(settingsmodal): move View to Nav.js and setup redux #142

--- Components/SettingsModal.js
+++ Components/SettingsModal.js
@@ -4,8 +4,7 @@ import { Modal, StyleSheet, Text, View, Pressable } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import { Picker } from "@react-native-picker/picker";
 
-const SettingsModal = ({ scene, gamePlayMode, setGamePlayMode }) => {
-  const [modalVisible, setModalVisible] = useState(false);
+const SettingsModal = ({ scene, gamePlayMode, setGamePlayMode, modalVisible, setModalVisible }) => {
 
   return (
     <View>
@@ -21,13 +20,6 @@ const SettingsModal = ({ scene, gamePlayMode, setGamePlayMode }) => {
               setModalVisible(!modalVisible);
             }}
           >
-            <View style={{
-              position: 'absolute',
-              height: "100%",
-              width: "100%",
-              opacity: 0.9,
-              backgroundColor: 'gray',
-            }} /> 
             <View style={styles.modalView}>
               <Pressable
                 style={styles.exit}
@@ -66,7 +58,8 @@ const SettingsModal = ({ scene, gamePlayMode, setGamePlayMode }) => {
 function mapStateToProps(state) {
   return {
     scene: state.scene,
-    gamePlayMode: state.gamePlayMode
+    gamePlayMode: state.gamePlayMode,
+    modalVisible: state.modalVisible,
   };
 }
 // this is how the picker tells redux that the user has selected a new player mode
@@ -78,6 +71,12 @@ function mapDispatchToProps(dispatch) {
         // type or action or action type, payload
         type: 'SET_GAME_PLAY_MODE',
         gamePlayMode: mode
+      })
+    },
+    setModalVisible: (visible) => {
+      dispatch({
+        type: 'SET_MODAL_VISIBLE',
+        modalVisible: visible
       })
     }
   }
