2ef2d3a223430730e5b6a8d4865814c24315fbfc
Habteab <keshi2017@gmail.com>
2022-04-08 10:45:44 -0700

    feat(lose game instruction): add pointer and circle animation

--- Components/HowToPlay/LoseGameInstruction.js
+++ Components/HowToPlay/LoseGameInstruction.js
@@ -21,30 +21,36 @@ export default function LoseGameInstruction() {
       y: 40,
     })
   ).current;
-  const lifePointerCircle = useRef(new Animated.Value(0)).current;
+  const lifePointerCircle = useRef(new Animated.Value(1)).current;
+  const lifePointerArrow = useRef(new Animated.Value(1)).current;
   const screenWidth = useWindowDimensions().width;
 
   useEffect(() => {
-    Animated.loop(
-      Animated.parallel([
-        Animated.timing(translation.x, {
-          toValue: Platform.OS === "web" ? 0 : 4,
-          duration: 2000,
-          delay: 1000,
-          useNativeDriver: false,
-        }),
-        Animated.timing(translation.y, {
-          toValue:
-            Platform.OS === "web" ? (width < 630 ? width * 0.13 : 120) : 100,
-          duration: 2000,
-          delay: 1000,
-          useNativeDriver: false,
-        }),
-        Animated.timing(lifePointerCircle, {
-          toValue: 1,
-          duration: 1000,
-          useNativeDriver: false,
-        }),
+    Animated.loop(        
+        Animated.parallel([
+          Animated.timing(translation.x, {
+            toValue: Platform.OS === "web" ? 0 : 4,
+            duration: 2000,
+            delay: 1000,
+            useNativeDriver: false,
+          }),
+          Animated.timing(translation.y, {
+            toValue:
+              Platform.OS === "web" ? (width < 630 ? width * 0.13 : 120) : 100,
+            duration: 2000,
+            delay: 1000,
+            useNativeDriver: false,
+          }),
+		  Animated.timing(lifePointerCircle, {
+			toValue: 0,
+			duration: 1500,
+			useNativeDriver: false,
+		  }),
+		  Animated.timing(lifePointerArrow, {
+			toValue: 0,
+			duration: 1500,
+			useNativeDriver: false,
+		  }),
       ])
     ).start();
   }, [width]);
@@ -62,20 +68,6 @@ export default function LoseGameInstruction() {
           aspectRatio: Platform.OS === "web" ? 1021 / 540 : 5 / 9,
         }}
       >
-        <Animated.View
-          style={{
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
-            transform: [
-              { translateX: translation.x },
-              { translateY: translation.y },
-            ],
-          }}
-        >
-          <Entypo name="mouse-pointer" size={18} color="black" />
-        </Animated.View>
-
         <Animated.Image
           source={
             Platform.OS === "web"
@@ -89,13 +81,55 @@ export default function LoseGameInstruction() {
             aspectRatio: Platform.OS === "web" ? 1021 / 540 : 5 / 9,
             borderRadius: 20,
             opacity: translation.x.interpolate({
-              delay: 10000,
+              delay: 1000,
+			  duration:10000,
               inputRange: Platform.OS === "web" ? [0, 2] : [4, 6],
-              outputRange: [1, 0],
+              outputRange: [0, 1],
               extrapolate: "clamp",
             }),
           }}
         />
+
+<Animated.View
+          style={{
+            transform: [
+              { translateX: Platform.OS === "web" ?screenWidth < 800
+              ? 270
+              : 240: 90 },
+              {
+                translateY:
+                  Platform.OS === "web"
+                    ? screenWidth < 800
+                      ? 230
+                      : 205
+                    : 280,
+              },
+            ],
+            opacity: lifePointerArrow,
+          }}
+        >
+          <Entypo name="mouse-pointer" size={18} color="black" />
+        </Animated.View>
+        <Animated.View
+          style={{
+            transform: [
+              { translateX: Platform.OS === "web" ? screenWidth < 800
+              ? 130
+              : 120 : 20 },
+              {
+                translateY:
+                  Platform.OS === "web"
+                    ? screenWidth < 800
+                      ? 115
+                      : 100
+                    : 200,
+              },
+            ],
+            opacity: lifePointerCircle,
+          }}
+        >
+          <Entypo name="circle" size={40} color="white" />
+        </Animated.View>
       </ImageBackground>
     </View>
   );
