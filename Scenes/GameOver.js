048b9a3c6ac7e84cedf24b118b1e413de2b1df7d
Jason Smith <jasons@alphaworks.tech>
2022-04-12 15:00:40 -0700

    fix(game over): add resizeMode of cover and height of 60% on milkywaybg image #147

--- Scenes/GameOver.js
+++ Scenes/GameOver.js
@@ -114,7 +114,7 @@ const styles = StyleSheet.create({
   },
   milkywaybg: {
     width: "100%",
-    height: "80%",
+    height: "60%",
     // aspectRatio: 468 / 272,
   },
   driveinforeground: {
