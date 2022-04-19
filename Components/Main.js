7d6011490ad40b3bb5cb88e1def4c0330265acc7
srosenberg74 <srosenberg@alphaworks.tech>
2022-04-19 14:17:00 -0700

    style(intro animation): add clapboard with text

--- Components/Main.js
+++ Components/Main.js
@@ -19,10 +19,16 @@ import { useFonts, Limelight_400Regular } from "@expo-google-fonts/limelight";
 import AppLoading from "expo-app-loading";
 
 function Main({ setScene, setMovies }) {
-  const clapperFade = useRef(new Animated.Value(1)).current;
-  const clapperFadeIn = useRef(new Animated.Value(0)).current;
+  const clapperOpenFade = useRef(new Animated.Value(1)).current;
+  const clapperClosedFade = useRef(new Animated.Value(0)).current;
   const textFade = useRef(new Animated.Value(0)).current;
+  const boardWithTextFade = useRef(new Animated.Value(0)).current;
   const [sound, setSound] = useState();
+
+  const { width, height } = useWindowDimensions();
+  useEffect(() => {
+    FetchApi().then((res) => setMovies(res));
+  }, []);
 
   async function playSound() {
     const { sound } = await Audio.Sound.createAsync(clapper, { volume: 0.1 });
@@ -31,35 +37,37 @@ function Main({ setScene, setMovies }) {
     await sound.playAsync();
   }
 
-  const { width } = useWindowDimensions();
-  useEffect(() => {
-    FetchApi().then((res) => setMovies(res));
-  }, []);
-
   useEffect(() => {
     setTimeout(() => {
       playSound();
-    }, 1400);
-    Animated.stagger(1497, [
-      Animated.timing(clapperFade, {
-        toValue: 0,
-        duration: 0,
-        delay: 1500,
+    }, 1540);
+    Animated.parallel([
+      Animated.timing(boardWithTextFade, {
+        toValue: 1,
+        duration: 3250,
         useNativeDriver: false,
       }),
-      Animated.parallel([
-        Animated.timing(clapperFadeIn, {
+      Animated.stagger(1497, [
+        Animated.timing(clapperOpenFade, {
+          toValue: 0,
+          duration: 0,
+          delay: 1500,
+          useNativeDriver: false,
+        }),
+        Animated.parallel([
+          Animated.timing(clapperClosedFade, {
+            toValue: 1,
+            duration: 2000,
+            useNativeDriver: false,
+          }),
+        ]),
+        Animated.timing(textFade, {
           toValue: 1,
-          duration: 2000,
+          delay: 800,
+          duration: 4000,
           useNativeDriver: false,
         }),
       ]),
-      Animated.timing(textFade, {
-        toValue: 1,
-        delay: 800,
-        duration: 4000,
-        useNativeDriver: false,
-      }),
     ]).start();
   }, []);
 
@@ -77,16 +85,18 @@ function Main({ setScene, setMovies }) {
             maxWidth: width,
             justifyContent: "center",
           }}
-          source={require("../Images/marquee.jpeg")}
-          resizeMode={Platform.OS === "web" ? "contain" : "cover"}
+          source={require("../Images/theater-sign-generator.jpg")}
+          resizeMode={width > 750 ? "contain" : "cover"}
           alt="movie theatre with marquee sign with cars parked in front"
         >
+          {/* Animated View for picture of open clapper with no text */}
           <Animated.View
             style={{
-              opacity: clapperFade,
+              opacity: clapperOpenFade,
               position: "absolute",
-              width: "85%",
+              width: width > 500 ? "85%" : "100%",
               alignSelf: "center",
+              alignItems: "center",
             }}
           >
             <Image
@@ -95,21 +105,23 @@ function Main({ setScene, setMovies }) {
                 zIndex: 100,
                 maxWidth: Platform.OS !== "web" ? "90%" : width,
               }}
-              source={require("../Images/clapper2-open.png")}
+              source={require("../Images/IntroAnimation/clapper2-open.png")}
               alt="open movie clapper"
               resizeMode="contain"
             />
           </Animated.View>
+          {/* Animated View for clapper image with arm closed with no text */}
           <Animated.View
             style={{
-              opacity: clapperFadeIn.interpolate({
+              opacity: clapperClosedFade.interpolate({
                 inputRange: [0, 0.0000000000000000001, 0.5, 1],
                 outputRange: [0, 1, 1, 0],
               }),
               position: "absolute",
-              width: "85%",
+              width: width > 500 ? "85%" : "100%",
               maxWidth: width,
               alignSelf: "center",
+              alignItems: "center",
             }}
           >
             <Image
@@ -118,15 +130,69 @@ function Main({ setScene, setMovies }) {
                 zIndex: 100,
                 maxWidth: Platform.OS !== "web" ? "90%" : width,
               }}
-              source={require("../Images/clapper2-closed.png")}
+              source={require("../Images/IntroAnimation/clapper2-closed.png")}
               alt="closed movie clapper"
               resizeMode="contain"
             />
           </Animated.View>
+          {/* Animated View for clapper board with actual text in foreground */}
+          <Animated.View
+            style={{
+              opacity: boardWithTextFade.interpolate({
+                inputRange: [0, 0.8, 1],
+                outputRange: [1, 1, 0],
+              }),
+              position: "absolute",
+              width: width > 500 ? "85%" : "100%",
+              maxWidth: width,
+              alignSelf: "center",
+              alignItems: "center",
+              zIndex: 101,
+            }}
+          >
+            <Image
+              style={{
+                aspectRatio: 1280 / 1117,
+                zIndex: 100,
+                maxWidth: Platform.OS !== "web" ? "90%" : width,
+              }}
+              //Can change font in following line to limelight, annie, or arial
+              source={require("../Images/IntroAnimation/clapper2-no-arm-limelight.png")}
+              alt="closed movie clapper"
+              resizeMode="contain"
+            />
+          </Animated.View>
+          {/* Animated View for the fading in title and start button */}
           <Animated.View style={{ opacity: textFade }}>
-            <View style={styles.titleContainer}>
-              <Text style={styles.fontText}>Trivia &#38; Chill</Text>
-              <View style={styles.buttonContainer}>
+            <View
+              style={[
+                styles.titleContainer,
+                { marginTop: Platform.OS !== "web" && width < 750 ? 64 : 0 },
+              ]}
+            >
+              <Text
+                style={[
+                  styles.fontText,
+                  {
+                    fontSize:
+                      height > 700
+                        ? width > 900
+                          ? 120
+                          : (100 * width) / 800
+                        : (100 * height) / 900,
+                  },
+                ]}
+              >
+                Trivia &#38; Chill
+              </Text>
+              <View
+                style={[
+                  styles.buttonContainer,
+                  {
+                    // I think it's okay without this line, but not positive.  It is causing some issues: paddingTop: width < 750 && Platform.OS === "web" ? 150 : 0,
+                  },
+                ]}
+              >
                 <ImageBackground
                   source={require("../Images/ticket.png")}
                   style={{
