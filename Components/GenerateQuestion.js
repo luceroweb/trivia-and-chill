d01f912052009be6ef81f87affad783099347ce1
Jason Smith <jasons@alphaworks.tech>
2022-04-20 15:44:53 -0700

    fix(error checking): add alert to checkUndefined helper function 163

--- Components/GenerateQuestion.js
+++ Components/GenerateQuestion.js
@@ -1,19 +1,18 @@
 import React, { useEffect } from "react";
-import { View } from "react-native";
+import { View, Alert } from "react-native";
 import madLibsArray from "../Utils/madLibsArray";
 import RandomGenerator from "../Utils/RandomGenerator";
 import { getGenreName } from "../Utils/FetchApi";
 import { connect } from "react-redux";
 import { getMovieChanges, getPerformerName } from "../Utils/FetchApi";
 import axios from "axios";
+import { Platform } from "expo-modules-core";
 
-const GenerateQuestion = ({ movies, setSelectedMovie }) => {
+const GenerateQuestion = ({ movies, setSelectedMovie, setScene}) => {
   let count = 0;
   const checkUndefined = () => {
-    console.log("RUNNING checkUndefined ", count);
     let movie = movies ? movies[RandomGenerator(movies.length)] : [];
     
-    movie.release_date = "";
     axios
       .all([getPerformerName(movie.id), getGenreName(movie.genre_ids[0])])
       .then(
@@ -22,18 +21,31 @@ const GenerateQuestion = ({ movies, setSelectedMovie }) => {
           let questionObject = movie ? madLibsArray(movie) : {};
           let randomIndex = RandomGenerator(questionObject.length);
           const movieQuestion = questionObject[randomIndex].question;
-          console.log("QUESTION 1: ", movieQuestion);
           const movieAnswer = questionObject[randomIndex].answer;
-          console.log("ANSWER 1: ", movieAnswer);
           if (count > 2) {
-            console.error("Count equals 3");
-            // add alert in here
+            if (Platform.OS === "web") {
+              alert(`ERROR: Found too much missing data.
+Press OK to return to the main screen.`);
+              setScene("Main");
+            }
+
+            if (Platform.OS !== "web") {
+              Alert.alert(
+                "ERROR: Found too much missing data.",
+                "Press OK to return to the main screen.",
+                [
+                  {
+                    text: "OK",
+                    onPress: () => setScene("Main"),
+                  }
+                ]
+              )
+            }
             return;
           }
           
           if (movieQuestion.includes("undefined")) {
             count++;
-            console.log("QUESTION MISSING DATA");
             checkUndefined();
             return;
           }
@@ -48,14 +60,13 @@ const GenerateQuestion = ({ movies, setSelectedMovie }) => {
 
             if (foundUndefined) {              
               count++;
-              console.log("ANSWER MISSING DATA");
               checkUndefined();
               return;
-            } else {
-              count = 0;
-              setSelectedMovie(questionObject[randomIndex]);
-              return;
             }
+
+            count = 0;
+            setSelectedMovie(questionObject[randomIndex]);
+            return;            
           }
 
           count = 0;
@@ -91,6 +102,11 @@ function mapDispatchToProps(dispatch) {
       dispatch({
         type: "SET_GENRE_NAME",
         genreName,
+      }),
+    setScene: (name) =>
+      dispatch({
+        type: "SET_SCENE",
+        name,
       }),
   };
 }
--- Utils/madLibsArray.js
+++ Utils/madLibsArray.js
@@ -72,40 +72,40 @@ export default function madLibsArray(movies) {
       //   answer: true,
       //   movieId: `${movies.id}`,
       // },
-      // {
-      //   question: `${randomCast1.name} starred in ${movies.title}.`, //get credits endpoint for performer name
-      //   answer: randomCast1.known_for_department === "Acting"
-      //     ? true
-      //     : false,
-      //   movieId: `${movies.id}`,
-      // },     
-      // {
-      //   question: `${movies.title}'s cast included ${randomCast1.name} .`, //get credits endpoint for performer name
-      //   answer: randomCast1.known_for_department === "Acting"
-      //     ? true
-      //     : false,
-      //   movieId: `${movies.id}`,
-      // },     
-      // {
-      //   question: `September 2, 1996 was the release date of Scream.`,
-      //   answer: false,
-      //   movieId: 4232,
-      // },
-      // {
-      //   question: `"A brillant and gifted young man must find the most beautiful location on the planet to set up a new vacation home for his family." describes what the movie Dune is about.`,
-      //   answer: false,
-      //   movieId: 438631,
-      // },
-      // {
-      //   question: `Aladdin is a movie about a young man in search of a magic carpet.`,
-      //   answer: false,
-      //   movieId: 812,
-      // },
-      // {
-      //   question: `La La Land's plot used to be described as "Two lovers are drawn together by their desire to do what is right.  They are faced with a growing need to dance into the night."`,
-      //   answer: false,
-      //   movieId: 313369,
-      // },
+      {
+        question: `${randomCast1.name} starred in ${movies.title}.`, //get credits endpoint for performer name
+        answer: randomCast1.known_for_department === "Acting"
+          ? true
+          : false,
+        movieId: `${movies.id}`,
+      },     
+      {
+        question: `${movies.title}'s cast included ${randomCast1.name} .`, //get credits endpoint for performer name
+        answer: randomCast1.known_for_department === "Acting"
+          ? true
+          : false,
+        movieId: `${movies.id}`,
+      },     
+      {
+        question: `September 2, 1996 was the release date of Scream.`,
+        answer: false,
+        movieId: 4232,
+      },
+      {
+        question: `"A brillant and gifted young man must find the most beautiful location on the planet to set up a new vacation home for his family." describes what the movie Dune is about.`,
+        answer: false,
+        movieId: 438631,
+      },
+      {
+        question: `Aladdin is a movie about a young man in search of a magic carpet.`,
+        answer: false,
+        movieId: 812,
+      },
+      {
+        question: `La La Land's plot used to be described as "Two lovers are drawn together by their desire to do what is right.  They are faced with a growing need to dance into the night."`,
+        answer: false,
+        movieId: 313369,
+      },
       {
         question: `When was ${movies.title} was released on?`, //movie details
         answer: [
@@ -124,77 +124,77 @@ export default function madLibsArray(movies) {
         ],
         movieId: `${movies.id}`,
       },
-      // {
-      //   question: `What was the release date of the ${movies.title}?`, //movie details
-      //   answer: [
-      //     `${convertedDate}`,
-      //     `${new generateRandomDate().toLocaleDateString("en-US")}`,
-      //     `${new generateRandomDate().toLocaleDateString("en-US")}`,
-      //   ],
-      //   movieId: `${movies.id}`,
-      // },
-      // {
-      //   question: `${movies.title} was released on what date?`, //movie details
-      //   answer: [
-      //     `${convertedDate}`,
-      //     `${new generateRandomDate().toLocaleDateString("en-US")}`,
-      //     `${new generateRandomDate().toLocaleDateString("en-US")}`,
-      //   ],
-      //   movieId: `${movies.id}`,
-      // },
-      // {
-      //   question: `When did ${movies.title} come out?`, //movie details
-      //   answer: [
-      //     `${convertedDate}`,
-      //     `${new generateRandomDate().toLocaleDateString("en-US")}`,
-      //     `${new generateRandomDate().toLocaleDateString("en-US")}`,
-      //   ],
-      //   movieId: `${movies.id}`,
-      // },      
-      // {
-      //   question: `What was the release date of ${movies.title}?`, //movie details
-      //   answer: [
-      //     `${convertedDate}`,
-      //     `${new generateRandomDate().toLocaleDateString("en-US")}`,
-      //     `${new generateRandomDate().toLocaleDateString("en-US")}`,
-      //   ],
-      //   movieId: `${movies.id}`,
-      // },
-      // {
-      //   question: `Ghostbusters was released on May 12, 1987.`,
-      //   answer: false,
-      //   movieId: 620,
-      // },
+      {
+        question: `What was the release date of the ${movies.title}?`, //movie details
+        answer: [
+          `${convertedDate}`,
+          `${new generateRandomDate().toLocaleDateString("en-US")}`,
+          `${new generateRandomDate().toLocaleDateString("en-US")}`,
+        ],
+        movieId: `${movies.id}`,
+      },
+      {
+        question: `${movies.title} was released on what date?`, //movie details
+        answer: [
+          `${convertedDate}`,
+          `${new generateRandomDate().toLocaleDateString("en-US")}`,
+          `${new generateRandomDate().toLocaleDateString("en-US")}`,
+        ],
+        movieId: `${movies.id}`,
+      },
+      {
+        question: `When did ${movies.title} come out?`, //movie details
+        answer: [
+          `${convertedDate}`,
+          `${new generateRandomDate().toLocaleDateString("en-US")}`,
+          `${new generateRandomDate().toLocaleDateString("en-US")}`,
+        ],
+        movieId: `${movies.id}`,
+      },      
+      {
+        question: `What was the release date of ${movies.title}?`, //movie details
+        answer: [
+          `${convertedDate}`,
+          `${new generateRandomDate().toLocaleDateString("en-US")}`,
+          `${new generateRandomDate().toLocaleDateString("en-US")}`,
+        ],
+        movieId: `${movies.id}`,
+      },
+      {
+        question: `Ghostbusters was released on May 12, 1987.`,
+        answer: false,
+        movieId: 620,
+      },
       // {
       //   question: `${movies.title}'s plot is described as "${movies.overview}"`,
       //   answer: true,
       //   movieId: `${movies.id}`,
       // },
-      // {
-      //   question: `${movies.title} is considered a ${movies.genre}`,
-      //   answer: true,
-      //   movieId: `${movies.id}`,
-      // },
-      // {
-      //   question: `${movies.title} is categorized under the ${movies.genre} genre.`,
-      //   answer: true,
-      //   movieId: `${movies.id}`,
-      // },
+      {
+        question: `${movies.title} is considered a ${movies.genre}`,
+        answer: true,
+        movieId: `${movies.id}`,
+      },
+      {
+        question: `${movies.title} is categorized under the ${movies.genre} genre.`,
+        answer: true,
+        movieId: `${movies.id}`,
+      },
       // {
       //   question: `"${movies.overview}" was the original plot of ${movies.title}.`,
       //   answer: true,
       //   movieId: `${movies.id}`,
       // },
-      // {
-      //   question: `Kill Bill is a romantic comedy.`,
-      //   answer: false,
-      //   movieId: 24,
-      // },
-      // {
-      //   question: `Captain America is categorized under the musicals genre.`,
-      //   answer: false,
-      //   movieId: 1771,
-      // },
+      {
+        question: `Kill Bill is a romantic comedy.`,
+        answer: false,
+        movieId: 24,
+      },
+      {
+        question: `Captain America is categorized under the musicals genre.`,
+        answer: false,
+        movieId: 1771,
+      },
       // {
       //   question: `Encanto's plot used to be described as "The Luceros are an extraordinary family who live hidden in the mountains of Mexico in a charmed place called the Encanto. The magic of the Encanto has blessed every child in the family with a unique gift -- every child except Miranda"`,
       //   answer: false,
@@ -210,26 +210,26 @@ export default function madLibsArray(movies) {
       //   answer: false,
       //   movieId: 8467,
       // },
-      // {
-      //   question: `Denzel Washington was in the movie Dude, Where's My Car?`,
-      //   answer: false,
-      //   movieId: 8859,
-      // },
-      // {
-      //   question: `Kristen Stewart was in Fight Club`,
-      //   answer: false,
-      //   movieId: 550,
-      // },
-      // {
-      //   question: `Olivia Munn starred in Ron's Gone Wrong`,
-      //   answer: false,
-      //   movieId: 482321,
-      // },
-      // {
-      //   question: `Ashton Kutcher starred in Grown Ups`,
-      //   answer: false,
-      //   movieId: 38365,
-      // },      
+      {
+        question: `Denzel Washington was in the movie Dude, Where's My Car?`,
+        answer: false,
+        movieId: 8859,
+      },
+      {
+        question: `Kristen Stewart was in Fight Club`,
+        answer: false,
+        movieId: 550,
+      },
+      {
+        question: `Olivia Munn starred in Ron's Gone Wrong`,
+        answer: false,
+        movieId: 482321,
+      },
+      {
+        question: `Ashton Kutcher starred in Grown Ups`,
+        answer: false,
+        movieId: 38365,
+      },      
     ]
   );
 }
