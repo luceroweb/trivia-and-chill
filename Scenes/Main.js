41c6f8ceacc4c0f21dc87715a0cea40c5bbe45a2
srosenberg74 <srosenberg@alphaworks.tech>
2022-04-27 14:33:20 -0700

    chore(images): add bak imgs and fix typo on clapper

--- Scenes/Main.js
+++ Scenes/Main.js
@@ -235,7 +235,7 @@ function Main({ setScene, setMovies }) {
                     width: IntroImageWidth,
                   },
                 ]}
-                source={require("../Images/Main/IntroAnimation/clapper2-no-arm-arial.png")}
+                source={require("../Images/Main/IntroAnimation/clapper2-no-arm-arial-revised.png")}
                 alt="closed movie clapper"
                 resizeMode="contain"
               />
